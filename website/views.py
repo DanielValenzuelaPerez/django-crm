940a1b63fb59523beb1567c79331d4bce43d7bb2
Daniel Valenzuela <DanielValenzuelaPerez@outlook.com>
2024-06-03 22:40:59 -0600

    Implemented login and logout functionality

--- website/views.py
+++ website/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+# https://getbootstrap.com/docs/5.3/components/alerts/
 
 
 def home(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have been logged in!')
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password not recognized! Try again.')
+            return redirect('home')
+
     return render(request, 'home.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You have been logged out...')
+    return redirect('home')
